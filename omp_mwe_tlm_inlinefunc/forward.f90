25a66c2d4a92bd21a198a1ab916b91f290d25c7d
Shreyas911 <shreyasg911@gmail.com>
2025-04-25 19:14:35 -0500

    Cleanup and renaming

---
+++ omp_mwe_tlm_inlinefunc/forward.f90
@@ -0,0 +1,48 @@
+module forward
+
+implicit none
+
+contains
+
+subroutine forward_problem(xx,V)
+
+!$ use omp_lib
+
+implicit none
+
+integer :: i
+real(8), intent(in), dimension(10000000) :: xx
+real(8), dimension(10000000) :: vi
+real(8) :: temp
+real(8), intent(out) :: V
+
+!$omp parallel do default(shared) private(i, temp)
+do i=1, 10000000
+    if (xx(i)**2 + xx(i)**3 .ge. 0) then
+
+        ! Inline function call
+        vi(i) = 1.0 + xx(i)*dummy_func(xx(i))
+
+        ! Not inline function call
+        temp = dummy_func(xx(i))
+        vi(i) = vi(i) + 1.0 + xx(i)*temp
+
+    end if
+end do
+!$omp end parallel do       
+
+V = SUM(vi)
+
+end subroutine forward_problem
+
+function dummy_func(x)
+
+implicit none
+real(8)             :: dummy_func
+real(8), intent(in) :: x
+
+dummy_func = x + 1.0
+
+end function dummy_func
+
+end module forward
